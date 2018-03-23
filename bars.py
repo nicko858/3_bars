3b169cfa8d3287e997c899678bc321c89e1f9ead
nicko858 <ikar2021@hotmail.com>
2018-03-23 15:43:05 +0300

    Упростил блок main

--- bars.py
+++ bars.py
@@ -16,9 +16,11 @@ def get_bar_by_size(parsed_data, size):
         bar_seats_count = x['properties']['Attributes']['SeatsCount']
         bar_size_dict[bar_name] = bar_seats_count
     if size == 'biggest':
-        return max(bar_size_dict, key=lambda seats_count: bar_size_dict[seats_count])
+        return max(bar_size_dict,
+                   key=lambda seats_count: bar_size_dict[seats_count])
     elif size == 'smallest':
-        return min(bar_size_dict, key=lambda seats_count: bar_size_dict[seats_count])
+        return min(bar_size_dict,
+                   key=lambda seats_count: bar_size_dict[seats_count])
 
 
 def get_closest_bar(parsed_data, longitude, latitude):
@@ -32,24 +34,35 @@ def get_closest_bar(parsed_data, longitude, latitude):
         bar_latitude = bar_coords_dict[bar_name][1]
         length_to_bar = (longitude - bar_longitude) ** 2 + (latitude - bar_latitude) ** 2
         length_to_bar_dict[bar_name] = length_to_bar
-    return min(length_to_bar_dict, key=lambda length_to_bar: length_to_bar_dict[length_to_bar])
+    return min(length_to_bar_dict,
+               key=lambda length_to_bar: length_to_bar_dict[length_to_bar])
 
 
-def check_is_or_less_null(value):
-    if value <= 0:
-       return True
+def check_is_or_less_null(value_to_check):
+    if value_to_check <= 0:
+        return True
     else:
-       return False
+        return False
 
 
-def keyboard_input(required):
-    if required == 'longitude':
-        print('To find the nearest bar for you, you have to enter your coordinates.\nPlease enter your longitude:')
+def keyboard_input(input_value):
+    if input_value == 'longitude':
+        print('To find the nearest bar for you, you have to enter your coordinates.\n'
+              'Please enter your longitude:')
         longitude = float(input())
+        if check_is_or_less_null(longitude):
+            exit('Entering data is incorrect! The correct format is {}\n'
+                 'Check the type of entering data! '
+                 'The {} is equal or less null! '.format(example_data, longitude))
         return longitude
-    elif required == 'latitude':
+    elif input_value == 'latitude':
         print('Please enter your latitude:')
         latitude = float(input())
+        if check_is_or_less_null(latitude):
+            print()
+            exit('Entering data is incorrect! The correct format is {}\n'
+                 'Check the type of entering data! '
+                 'The {} is equal or less null! '.format(example_data, latitude))
         return latitude
 
 
@@ -69,15 +82,11 @@ if __name__ == '__main__':
         exit('Incorrect line argument!\nUsing: {}'.format(script_usage))
     longitude = keyboard_input('longitude')
     latitude = keyboard_input('latitude')
-    for coord in (longitude, latitude):
-        if check_is_or_less_null(coord):
-            print('Entering data is incorrect! The correct format is {}'.format(example_data))
-            exit('Check the type of entering data! The {} is equal or less null! '.format(coord))
     try:
         json_content = load_data(sys.argv[1])
     except ValueError:
-        print('Decoding JSON has failed!')
-        exit('The source-file is not a valid JSON! Check the file content!')
+        exit('Decoding JSON has failed!\n'
+             'The source-file is not a valid JSON! Check the file content!')
     print_content('biggest')
     print_content('smallest')
     print_content('closest', longitude, latitude)
