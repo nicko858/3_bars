3832efd01d2c3751462198b8255e78d06a6ffadc
nicko858 <ikar2021@hotmail.com>
2018-04-04 17:38:26 +0300

    Сократил функции

--- bars.py
+++ bars.py
@@ -1,5 +1,7 @@
 import json
 import argparse
+import sys
+import re
 
 
 def load_data(filepath):
@@ -43,30 +45,15 @@ def get_closest_bar(parsed_data, longitude, latitude):
 
 
 def keyboard_input(input_value):
-    example_data = 'longitude=37.621587946152012 latitude=55.765366956608361'
-    value_error_message = ('Entering value is incorrect! '
-                           'The correct format is float: {}').format(example_data)
     if input_value is 'longitude':
         print('To find the nearest bar for you, you have to enter your coordinates.\n'
               'Please enter your longitude:')
-        while True:
-            try:
-                longitude = float(input())
-                return longitude
-            except ValueError:
-                print(value_error_message)
-                print('Please enter your longitude:')
-                continue
+        longitude = float(input())
+        return longitude
     elif input_value is 'latitude':
         print('Please enter your latitude:')
-        while True:
-            try:
-                longitude = float(input())
-                return longitude
-            except ValueError:
-                print(value_error_message)
-                print('Please enter your latitude:')
-                continue
+        longitude = float(input())
+        return longitude
 
 
 def print_content(required, longitude=0.0, latitude=0.0):
@@ -93,17 +80,24 @@ def get_args():
 
 
 if __name__ == '__main__':
-    example_data = 'longitude=37.621587946152012 latitude=55.765366956608361'
     args = get_args()
+    load_data_exit_message = "No such file or directory {}".format(args.source_data)
+    get_json_content_exit_message = 'The source-file is not a valid JSON or empty! Check the file content!'
+    example_data = 'longitude=37.621587946152012 latitude=55.765366956608361'
+    error_dict = {
+        'classjsondecoderJSONDecodeError': 'The source-file is not a valid JSON or empty! Check the file content!',
+        'classFileNotFoundError': 'No such file or directory {}'.format(args.source_data),
+        'classValueError': 'Entering value is incorrect! '
+                           'The correct format is float: {}'.format(example_data)
+    }
     try:
         file_to_load = load_data(args.source_data)
-    except IOError:
-        exit("No such file or directory {}".format(args.source_data))
-    try:
         json_content = get_json_content(file_to_load)
-    except ValueError:
-        exit('The source-file is not a valid JSON or empty! Check the file content!')
-    longitude = keyboard_input('longitude')
-    latitude = keyboard_input('latitude')
+        longitude = keyboard_input('longitude')
+        latitude = keyboard_input('latitude')
+    except (IOError, ValueError):
+        error = str(sys.exc_info()[0])
+        error_text = re.sub('\W+', '', error)
+        exit(error_dict.get(error_text))
     for required_content in ['biggest', 'smallest', 'closest']:
         print_content(required_content, longitude, latitude)
